34318d2f5b1237bf3647e2260612ef3a1d138de0
InvincibleAditya <invincibleaditya043@gmail.com>
2026-01-29 03:05:22 +0530

    please push th new file

---
+++ myworld.java
@@ -0,0 +1,10 @@
+public class myworld {
+    public static void main(String[] args) {
+        String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        
+        System.out.println("Days of the Week:");
+        for (String day : days) {
+            System.out.println(day);
+        }
+    }
+}
